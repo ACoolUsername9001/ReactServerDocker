d97099bd80b1d6f2203ea5c3244f578b23efbe17
acoolname <acoolname@acooldomain.co>
2023-09-19 08:11:56 +0300

    working

--- App.tsx
+++ App.tsx
@@ -1,49 +1,99 @@
-import ServersBoard, { ArgumentType, CommandInfo, CommandType, OpenCommand, SendCommand, ServerItem as ServerComponent, ServerInfo, StreamCommand, getCommands, setCommands } from "./buttons_menu";
+import ServersBoard from "./buttons_menu";
 import "./app.module.css"
+import axios, { AxiosInstance } from "axios";
+import React, { useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+const api: AxiosInstance = axios.create({
+  baseURL: 'http://127.0.0.1:8000',
+});
 
-
-function loadServers(): { [server_id: string]: ServerInfo } {
-  return {
-    'some-id1': {
-      name: 'ACoolMinecraftServer1',
-      on: false,
-    },
-    'some-id2': {
-      name: 'ACoolMinecraftServer2',
-      on: false,
-    },
-    'some-id3': {
-      name: 'ACoolMinecraftServer3',
-      on: true,
-    },
-    'some-id4': {
-      name: 'ACoolMinecraftServer4',
-      on: false,
-    },
-    'some-id5': {
-      name: 'ACoolMinecraftServer5',
-      on: true,
-    },
-    'some-id6': {
-      name: 'ACoolMinecraftServer6',
-      on: false,
-    },
+// Function to obtain the token
+const fetchToken = async (username: string, password: string) => {
+  try {
+    const response = await api.post('/token', {
+      grant_type: 'password',
+      username: username,
+      password: password,
+      client_id: null,
+      client_secret: null,
+    }, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+    return response.data.access_token;
+  } catch (error) {
+    console.error('Error fetching token:', error);
+    throw error;
   }
-}
+};
 
-function loadCommands() {
-  const commands: CommandInfo[] = []
-  commands.push(new SendCommand('Start', { 'ports': ArgumentType.String }))
-  commands.push(new StreamCommand('Interactive Shell', null))
-  commands.push(new OpenCommand('File Browser', null))
-  commands.push(new SendCommand('Run Command', { 'command': ArgumentType.String }))
-  return commands
-}
+// api.interceptors.request.use(
+//   async (config) => {
+//     const token = await fetchToken('ACoolName', 'Test');
+//     config.headers.Authorization = `Bearer ${token}`;
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
+
+function LoginPage(props: { setApiAuthenticated: (api: boolean) => void }) {
+  const [username, setUsername] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [apiPatched, setApiPatched] = useState(false)
+  const handleSubmit = () => {
+    fetchToken(username, password).then(
+      (token) => {
+        api.interceptors.request.use(
+          async (config) => {
+            config.headers.Authorization = `Bearer ${token}`;
+            return config
+          }
+        );
+        setApiPatched(true)
+        props.setApiAuthenticated(true)
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    )
+  }
+  if (apiPatched) {
+    return <Navigate to='/' />
+  }
+
+  return (
+    <div className="login-container">
+      <div>
+        <form className='text' onSubmit={handleSubmit}>
+          <div>
+            Username: <input type='text' name='username' onChange={(event) => { setUsername(event.target.value) }} />
+          </div>
+          <div>
+            Password:<input type='password' name='password' onChange={(event) => { setPassword(event.target.value) }} />
+          </div>
+          <input type="button" value="submit" onClick={handleSubmit}></input>
+        </form>
+      </div>
+    </div>
+  )
+}
 
 export default function App() {
-  const servers = loadServers()
-  const commands = loadCommands()
-  return ServersBoard(servers, commands)
+  const [apiAuthenticated, setApiAuthenticated] = useState(false)
+  console.log(apiAuthenticated)
+  return (<div className="container">
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate to='/servers' />} />
+        <Route path='/login' element={<LoginPage setApiAuthenticated={setApiAuthenticated} />} />
+        <Route path='/servers' element={ServersBoard(api, apiAuthenticated, () => { return <Navigate to='/login' /> })}> </Route>
+      </Routes>
+    </BrowserRouter>
+  </div >
+  )
 }
--- buttons_menu.tsx
+++ buttons_menu.tsx
@@ -1,6 +1,7 @@
-import React, { ReactElement, useState } from "react"
+import React, { Context, ReactElement, createContext, useContext, useEffect, useState } from "react"
 import { View, StyleSheet, Text, ViewStyle, } from "react-native";
 import "./app.module.css"
+import axios, { AxiosInstance } from 'axios';
 
 export enum ArgumentType {
     Number = "Number",
@@ -11,8 +12,10 @@ export enum ArgumentType {
 
 export enum CommandType {
     Browse = 'Browse',
-    Send = 'Send',
-    Stream = 'Stream',
+    StartServer = 'Start Server',
+    RunCommand = 'RunCommand',
+    Stop = 'Stop',
+    Delete = 'Delete',
 }
 
 export class CommandInfo {
@@ -28,26 +31,11 @@ export class CommandInfo {
 
 
 export class SendCommand extends CommandInfo {
-    command_type = CommandType.Send
+    command_type = CommandType.StartServer
 
     constructor(name: string, args: { [argument_id: string]: ArgumentType }) {
-        super(name = name, args = args, CommandType.Send)
+        super(name = name, args = args, CommandType.StartServer)
         this.args = args
-    }
-}
-
-export class StreamCommand extends CommandInfo {
-    command_type = CommandType.Stream
-    constructor(name: string, args: { [argument_id: string]: ArgumentType }) {
-        super(name = name, args = args, CommandType.Stream)
-    }
-}
-
-
-export class OpenCommand extends CommandInfo {
-    command_type = CommandType.Browse
-    constructor(name: string, args: { [argument_id: string]: ArgumentType }) {
-        super(name = name, args = args, CommandType.Browse)
     }
 }
 
@@ -60,48 +48,144 @@ function StatusRow(props: { text: string, on: boolean }) {
 }
 
 
-function Command(props: { command: CommandInfo }) {
+function Command(props: { command: CommandInfo, handler: () => void }) {
 
 
-    return (<button className="command">
+    return (<button className="command" onClick={props.handler}>
         <a className="commandText"> {props.command.name}</a>
     </button>)
 }
 
 
-function ServerItem(props: { name: string, on: boolean, id: string, commands: CommandInfo[] }) {
-    const commandsElements: ReactElement[] = [];
-    for (let command of props.commands) {
-        commandsElements.push(Command({ command: command }))
-    }
+function ServerItem(props: { name: string, on: boolean, id: string, commands: Element[] }) {
     return (
-        <div className="server" id={props.id}>
+        <div className="server" id={props.id} key={props.id}>
             <StatusRow on={props.on} text={props.name} />
             <div className="commandsGrid">
-                {commandsElements}
+                {props.commands}
             </div>
         </div>
     )
 }
 
 
-
-export class ServerInfo {
-    name: string
-    on: boolean
+enum Protocol {
+    TCP = 'tcp',
+    UDP = 'udp'
 }
 
 
-export default function ServersBoard(servers: { [server_id: string]: ServerInfo }, commands: CommandInfo[]) {
-    const serverComponents = []
-    for (let [server_id, info] of Object.entries(servers)) {
-        serverComponents.push(ServerItem({ id: server_id, on: info.on, name: info.name, commands: commands }))
+class Port {
+    number: number
+    protocol: Protocol
+}
+
+
+class ImageInfo {
+    name: string
+    version: string
+    ports: Port[]
+}
+
+
+class ServerInfo {
+    id_: string
+    name: string
+    on: boolean
+    user_id: string
+    image: ImageInfo
+    ports: Port[] | null
+    domain: string
+}
+
+async function loadServers(api: AxiosInstance): Promise<{ status: number, data: ServerInfo[] }> {
+    let response = await api.get('/servers')
+    return {
+        status: response.status,
+        data: response.data
     }
-    return (
-        <div className="container">
-            <div className="grid">
-                {serverComponents}
+
+}
+
+
+function loadCommands() {
+    const commands: CommandInfo[] = []
+    commands.push(new CommandInfo('Start', { 'ports': ArgumentType.String }, CommandType.StartServer))
+    commands.push(new CommandInfo('Stop', { 'ports': ArgumentType.String }, CommandType.Stop))
+    // commands.push(new StreamCommand('Interactive Shell', null))
+    // commands.push(new OpenCommand('File Browser', null))
+    // commands.push(new SendCommand('Run Command', { 'command': ArgumentType.String }))
+    return commands
+}
+
+
+function createHandler(api: AxiosInstance, server_id: string, command: CommandInfo): () => void {
+    switch (command.command_type) {
+        case CommandType.StartServer: {
+            return async () => {
+                return (await api.post(`/servers/${server_id}/start`)).data
+            }
+        }
+        case CommandType.Delete: {
+            return async () => { return (await api.delete(`/servers/${server_id}`)).data }
+        }
+        case CommandType.Browse: {
+            return async () => { return (await api.post(`/servers/${server_id}/browse`)).data }
+        }
+        case CommandType.Stop: {
+            return async () => { return (await api.post(`/servers/${server_id}/stop`)).data }
+        }
+    }
+}
+
+
+export default function ServersBoard(api: AxiosInstance, apiAuthenticated, onFail: () => any) {
+    let commands = loadCommands()
+    const [servers, setServers] = useState([]);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (apiAuthenticated) {
+                let servers_promised = loadServers(api)
+                servers_promised.then((response) => {
+                    setServers(response.data)
+                })
+                    .catch(
+                        (error) => {
+                            console.log('Failed to get servers: ' + error);
+                            if (error.response) {
+                                if (error.response.status == 401) {
+                                    setServers(null)
+                                }
+                            }
+                        }
+                    )
+            }
+        }, 5000
+        );
+        return () => { clearInterval(interval) }
+    }, [apiAuthenticated])
+
+    if (servers == null) {
+        setServers([]);
+        return (
+            <div>
+                {onFail()}
             </div>
+        )
+    }
+    if (!apiAuthenticated) {
+        return onFail();
+    }
+    servers.sort((s1: ServerInfo, s2: ServerInfo) => { return s1.id_ < s2.id_ ? 0 : 1 })
+    return (
+        <div className="grid">
+            {
+                servers.map(
+                    (value: ServerInfo, index: number, array) => {
+                        return ServerItem({ name: value.user_id + '`s ' + value.image.name + ' ' + value.image.version + ' Server', on: value.on, id: value.id_, commands: commands.map((command, i, array) => { return Command({ command: command, handler: createHandler(api, value.id_, command) }) }) })
+                    }
+                )
+            }
         </div>
     );
 }
