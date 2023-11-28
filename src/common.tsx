c43a28b5caeebe0305d60f0f04e9e9798e2adc81
ACoolName <acoolname@acooldomain.co>
2023-11-28 14:36:45 +0200

    changed button layout for actions

--- src/common.tsx
+++ src/common.tsx
@@ -10,6 +10,13 @@ import { OpenAPISchema } from "./interfaces";
 import { JSONSchema7 } from "json-schema";
 import { IChangeEvent } from "@rjsf/core";
 import { RJSFSchema, WidgetProps } from "@rjsf/utils";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Popper from '@mui/material/Popper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+
 
 // import TextWidget from '@rjsf/core/src/components/widgets/TextWidget';
 
@@ -256,12 +263,91 @@ export function ActionItem(p: { action: ActionInfo, identifierSubstring?: string
 }
 
 export function ActionGroup(p: { actions: ActionInfo[], identifierSubstring?: string, children?: ReactNode}) {
+    const actionItems: any[] = p.actions.map((action, index, array) => (<ActionItem action={action} identifierSubstring={p.identifierSubstring} /> ))
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef<HTMLDivElement>(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    
+    for (let child in React.Children.toArray(p.children)){
+        actionItems.push(child)
+    }
+    console.log(actionItems)
+
+    const handleMenuItemClick = (
+        event: React.MouseEvent<HTMLLIElement, MouseEvent>,
+        index: number,
+      ) => {
+        setSelectedIndex(index);
+        setOpen(false);
+    };
+
+    const handleToggle = () => {
+      setOpen((prevOpen) => !prevOpen);
+    };
+
+    const handleClose = (event: Event) => {
+      if (
+        anchorRef.current &&
+        anchorRef.current.contains(event.target as HTMLElement)
+      ) {
+        return;
+      }
+  
+      setOpen(false);
+    }
     return (
-        <ButtonGroup variant="outlined" aria-label="outlined button group">
-            {p.actions.map((action, index, array) => { return <ActionItem action={action} identifierSubstring={p.identifierSubstring} /> })}
-            {p.children}
-        </ButtonGroup>
-    )
+        <React.Fragment>
+          <ButtonGroup variant="outlined" ref={anchorRef} aria-label="split button">
+            {actionItems[selectedIndex]}
+            <Button
+          size="small"
+          aria-controls={open ? 'split-button-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          onClick={handleToggle}
+        >
+          <ArrowDropDownIcon />
+        </Button>
+          </ButtonGroup>
+          <Popper
+            sx={{
+              zIndex: 1,
+            }}
+            open={open}
+            anchorEl={anchorRef.current}
+            role={undefined}
+            transition
+            disablePortal
+          >
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{
+                  transformOrigin:
+                    placement === 'bottom' ? 'center top' : 'center bottom',
+                }}
+              >
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList id="split-button-menu" autoFocusItem>
+                      {actionItems.map((option, index) => (
+                        <MenuItem
+                          key={option.props.action.name}
+                          selected={index === selectedIndex}
+                          onClick={(event) => handleMenuItemClick(event, index)}
+                        >
+                          {option.props.action.name}
+                        </MenuItem>
+                      ))}
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </React.Fragment>
+      );
 }
 
 
